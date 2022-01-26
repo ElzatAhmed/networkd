8d11b0d49324b3ed759b7481fda4318921348404
Elzat Ahmed <elzat_ahmed@163.com>
2022-01-26 17:23:29 +0800

    new ignores

--- networkd/visualization/visualize.py
+++ networkd/visualization/visualize.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import networkx as nx
 from random import random
+from pyvis.network import Network as pyNet
 
 from networkd.classes.networks import DisNet
 
@@ -24,6 +25,26 @@ def plot(
     else:
         matplotlib.use("Agg")
         figure.savefig(save_path)
+
+
+def toHtml(
+        network: DisNet, pos=None, save_path='network.html',
+        node_color='blue', show_buttons=False
+):
+    nodes = network.nodes
+    edges = network.edges
+    pynet = pyNet(
+        height='700px', width='1500px', notebook=True, layout=False, directed=False
+    )
+    pos = _random_pos(network.xGraph) if pos is None else pos
+    for node in nodes:
+        pynet.add_node(node, color=node_color, x=pos[node][0], y=pos[node][1])
+    for edge in edges:
+        pynet.add_edge(source=edge[0], to=edge[1])
+    if show_buttons:
+        pynet.width = 1000
+        pynet.show_buttons()
+    pynet.show()
 
 
 def _random_pos(graph: nx.Graph):
